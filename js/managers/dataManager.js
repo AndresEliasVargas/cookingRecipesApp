87056f8ff8bdf5eeafa5710e84524920ad9e8c41
AndresEliasVargas <andreselias.vargas@gmail.com>
2019-08-13 21:23:19 -0600

    feat
    New Documents JS
    New dataManager functionability

--- js/managers/dataManager.js
+++ js/managers/dataManager.js
@@ -20,14 +20,49 @@ class DataManager {
 
     getRecipesCallback(e) {
         let request = e.target;
+        let notes = null;
 
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 let dataRecipes = JSON.parse(request.response);
-                console.log(dataRecipes);
+                console.log(dataRecipes.recipe);
 
-                
+                dataRecipes.recipe.map(dataRecipe => {
+                    console.log(dataRecipe);
+
+                    let ingredients = new Ingredient(
+                        dataRecipe.ingredient.amount,
+                        dataRecipe.ingredient.unit,
+                        dataRecipe.ingredient.name
+                    );
+
+                    // let step = new Step();
+
+                    // let tag = new Tag();
+
+                    if(dataRecipe.notes === undefined || dataRecipe.notes === '' || dataRecipe.notes === null){
+                        notes;
+                    } else {
+                        notes = dataRecipe.notes;
+                    };
+
+                    let newRecipe = new Recipe(
+                        dataRecipe.id,
+                        dataRecipe.name,
+                        dataRecipe.description,
+                        dataRecipe.forked,
+                        dataRecipe.image,
+                        notes,
+                        ingredients
+                    );
+
+                    this.recipes.push(newRecipe);
+                });
             };
+            console.log(this.recipes);
+            
         };
+
     };
+
 };
---
+++ js/models/ingredient.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class Ingredient{
+    constructor(){
+
+    };
+    
+};
